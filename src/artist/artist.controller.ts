a589efb4d2c5e11b31689f8324ceba5882a44bcd
Nickolay Malysh <stalcrot@yandex.ru>
2022-07-17 17:19:45 +0300

    feat: add artist entity

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
@@ -7,27 +16,30 @@ import { UpdateArtistDto } from './dto/update-artist.dto';
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
-  @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
-  }
-
   @Get()
   findAll() {
     return this.artistService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.artistService.findOne(+id);
   }
 
+  @Post()
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ) {
     return this.artistService.update(+id, updateArtistDto);
   }
 
-  @Delete(':id')
+  @Delete(':id', ParseUUIDPipe)
   remove(@Param('id') id: string) {
     return this.artistService.remove(+id);
   }
--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,1 +1,7 @@
-export class Track {}
+export class Track {
+  id: string; // uuid v4
+  name: string;
+  artistId: string | null; // refers to Artist
+  albumId: string | null; // refers to Album
+  duration: number; // integer number
+}
