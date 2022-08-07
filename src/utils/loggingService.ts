472329a459fef100eaa26ef1a3b6659427ce08d0
Nickolay Malysh <stalcrot@yandex.ru>
2022-08-07 22:42:58 +0300

    feat: implement date for logging

--- src/utils/loggingService.ts
+++ src/utils/loggingService.ts
@@ -1,17 +1,20 @@
 import { ConsoleLogger, LogLevel } from '@nestjs/common';
+import { writeLog } from './loggingFiles';
 
 export class MyLogger extends ConsoleLogger {
   error(message: any, stack?: string, context?: string) {
-    // const args = [message, stack, context] as const;
     super.error(message);
+    writeLog(message);
   }
 
   warn(message: any, stack?: string, context?: string) {
     super.warn(message);
+    writeLog(message);
   }
 
   log(message: any, stack?: string, context?: string) {
     super.log(message);
+    writeLog(message);
   }
 }
 
